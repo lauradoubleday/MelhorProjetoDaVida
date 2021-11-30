6f6535a8e0a411faaa9903f789931d2c309251cd
Laura Doubleday <80928970+lauradoubleday@users.noreply.github.com>
2021-11-29 23:27:53 -0300

    Add files via upload

---
+++ Ui/Frm_Cadastro.Designer.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MelhorProjetoDaVida.Ui
+{
+    partial class Frm_Cadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.dgvCadastro = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(121, 41);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(100, 20);
+            this.txtCpf.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(15, 41);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.White;
+            this.btnCadastrar.Location = new System.Drawing.Point(77, 90);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(84, 23);
+            this.btnCadastrar.TabIndex = 2;
+            this.btnCadastrar.Text = "CADASTRAR";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(12, 25);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(39, 13);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "NOME";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(333, 41);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(227, 41);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(118, 25);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(27, 13);
+            this.lblCpf.TabIndex = 6;
+            this.lblCpf.Text = "CPF";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(227, 25);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(39, 13);
+            this.lblEmail.TabIndex = 7;
+            this.lblEmail.Text = "EMAIL";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(330, 25);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(44, 13);
+            this.lblSenha.TabIndex = 8;
+            this.lblSenha.Text = "SENHA";
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(290, 90);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(84, 23);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "EXCLUIR";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click_1);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(182, 90);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(84, 23);
+            this.btnEditar.TabIndex = 10;
+            this.btnEditar.Text = "EDITAR";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click_1);
+            // 
+            // dgvCadastro
+            // 
+            this.dgvCadastro.BackgroundColor = System.Drawing.Color.White;
+            this.dgvCadastro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCadastro.GridColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dgvCadastro.Location = new System.Drawing.Point(15, 144);
+            this.dgvCadastro.Name = "dgvCadastro";
+            this.dgvCadastro.Size = new System.Drawing.Size(418, 150);
+            this.dgvCadastro.TabIndex = 11;
+            this.dgvCadastro.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCadastro_CellContentClick);
+            // 
+            // Frm_Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvCadastro);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCpf);
+            this.Name = "Frm_Cadastro";
+            this.Text = "Frm_Cadastro";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void dgvCadastro_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            CadastroDTO.Nome1 = txtNome.Text;
+            CadastroDTO.CPF1 = int.Parse(txtCpf.Text);
+            CadastroDTO.Email1 = txtEmail.Text;
+            CadastroDTO.Senha1 = txtSenha.Text;
+
+            CadastroBLL.Inserir(CadastroDTO);
+
+            MessageBox.Show("Cadastrado com sucesso!", "Usuário", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            txtNome.Clear();
+            txtCpf.Clear();
+            txtEmail.Clear();
+            txtSenha.Clear();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.DataGridView dgvCadastro;
+    }
+}
---
+++ Ui/Frm_Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MelhorProjetoDaVida.CODE.BLL;
+using MelhorProjetoDaVida.CODE.DTO;
+
+namespace MelhorProjetoDaVida
+{
+    public partial class Frm_Login : Form
+    {
+        LoginDTO LoginDTO = new LoginDTO();
+        LoginBLL LoginBLL = new LoginBLL();
+        public Frm_Login()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            LoginDTO.Cpf = int.Parse(txtCpf.Text);
+            LoginDTO.Email = txtEmail.Text;
+            LoginDTO.Senha = txtSenha.Text;
+
+            if (LoginBLL.RealizarLogin(LoginDTO) == true)
+            {
+                Frm_Login Frm_Login = new Frm_Login();
+                Frm_Login.ShowDialog();
+            }
+
+           else
+            {
+                {
+                    MessageBox.Show("Confira se há algo errado.", "Há algo errado.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+           
+
+        }
+    }
+}
