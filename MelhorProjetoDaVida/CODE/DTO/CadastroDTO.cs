4e9538af5b53a874f1cbd787cf3dd5f9a8c6c33c
Laura Doubleday <80928970+lauradoubleday@users.noreply.github.com>
2021-11-30 17:31:52 -0300

    Add files via upload

---
+++ MelhorProjetoDaVida/CODE/DTO/CadastroDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelhorProjetoDaVida.CODE.DTO
+{
+    class CadastroDTO
+    {
+        private string nome;
+        private int cpf;
+        private string email;
+        private string senha;
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Cpf { get => cpf; set => cpf = value; }
+        public string Email { get => email; set => email = value; }
+        public string Senha { get => senha; set => senha = value; }
+    }
+}
